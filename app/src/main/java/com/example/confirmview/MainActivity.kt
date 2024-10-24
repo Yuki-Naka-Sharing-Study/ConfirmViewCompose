b863bf2e7882e312f3dd31d646d4f5d7eb493b25
Yuki Naka <yukinaka2168@au.com>
2024-10-24 09:58:17 +0900

    「Box」を使用し、「NoRecordImageView」を画面の真ん中で表示することに成功。

--- app/src/main/java/com/example/confirmview/MainActivity.kt
+++ app/src/main/java/com/example/confirmview/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,14 +24,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             ConfirmViewTheme {
                 Surface(
-//                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoRecordImageView(
-                        modifier = Modifier
-                            .size(100.dp)
-                            .aspectRatio(1f)
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        NoRecordImageView(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .aspectRatio(1f)
+                        )
+                    }
                 }
             }
         }
